f59343d1e65b3c4fc3f28f98fe120d56c2844e17
Hiruni Ranasinghe <139541233+HiruniRanasinghe@users.noreply.github.com>
2024-11-19 14:56:36 +0000

    fix some design

--- studentapp/src/App.js
+++ studentapp/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>Students Information Portal</h1>
       <hr></hr>
-      <table>
+      <table width="100%">
         <tbody>
           <tr>
             <td>
